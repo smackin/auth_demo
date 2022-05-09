6c004887aba5599d3014049c82df3c27ef0f2558
Stephanie <smmackin12@gmail.com>
2022-05-09 13:09:22 -0400

    first commit

---
+++ forms.py
@@ -0,0 +1,11 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+from wtforms.validators import InputRequired
+
+class UserForm(FlaskForm):
+    username = StringField("Username", validators=[InputRequired()])
+    password = PasswordField("Password", validators=[InputRequired()])
+    
+    
+class TweetForm(FlaskForm):
+    text = StringField("Enter your tweet", validators=[InputRequired])
